9d09ef80671205f34f3806009885f3f062a02ff1
PaBoba <140555335+PaBoba@users.noreply.github.com>
2023-12-06 16:34:21 -0800

    added utils folder and helpers.js file and updated server.js

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const handlebars = require("express-handlebars");
 const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers.js");
+const sequelize = require("./config/connection");
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+
+// Create the Handlebars.js engine object with custom helper functions
+const hbs = exphbs.create({ helpers });
+
+// Inform Express.js which template engine we're using
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => console.log(`Server started at http://localhost:PORT`));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
